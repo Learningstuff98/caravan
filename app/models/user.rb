3e3a31e08ff891fc0109367630dbc5124fcaf13c
Andy Strube <andystrube64@gmail.com>
2020-09-11 18:17:28 -0700

    Added validations for usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   attr_accessor :login
+  validates :username, presence: true, length: { minimum: 3 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
