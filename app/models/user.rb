54152cc18936b397a46e52678f6261b3240bd9a1
Andy Strube <andystrube64@gmail.com>
2020-09-11 18:31:44 -0700

    Usernames are unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   attr_accessor :login
-  validates :username, presence: true, length: { minimum: 3 }
+  validates :username, uniqueness: true, presence: true, length: { minimum: 3 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
