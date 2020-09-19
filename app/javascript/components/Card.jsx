42b4353085e9f3a3b35571278a791b247eee4129
Andy Strube <andystrube64@gmail.com>
2020-09-18 23:35:14 -0700

    Added more styling to the card component

--- app/javascript/components/Card.jsx
+++ app/javascript/components/Card.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
 
-{/* <h1 className="black">{"♣"}</h1>
-<h1 className="red">{"♦"}</h1>
-<h1 className="red">{"♥"}</h1>
-<h1 className="black">{"♠"}</h1> */}
-
 export default function Card({ card }) {
-  return <div className='card'>
-    
-  </div>  
+
+  const handleSuit = () => {
+    if(card.suit === "Clubs") {
+      return <span className="black">♣</span>
+    }
+    if(card.suit === "Spades") {
+      return <span className="black">♠</span>
+    }
+    if(card.suit === "Diamonds") {
+      return <span className="red">♦</span>
+    }
+    return <span className='red'>♥</span>
+  };
+
+  const handleValue = () => {
+    if(card.value) {
+      if(card.value === 1) {
+        return <span>A</span>
+      }
+      return <span>{card.value}</span>
+    }
+  };
+
+  const handleColor = () => {
+    if(['Hearts', 'Diamonds'].includes(card.suit)) {
+      return 'red';
+    }
+    return 'black';
+  };
+
+  return <div className={`card ${handleColor()}`}>
+    <span className="upper-number">
+      {handleValue()}
+    </span>
+    <span className="lower-number">
+      {handleValue()}
+    </span>
+  </div>
 }
