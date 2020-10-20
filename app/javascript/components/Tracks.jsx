4808e4511cc20dda4a46947c837bc60960586386
Andy Strube <andystrube64@gmail.com>
2020-10-19 19:28:37 -0700

    Refactored the tracks component

--- app/javascript/components/Tracks.jsx
+++ app/javascript/components/Tracks.jsx
@@ -2,27 +2,12 @@ import React from 'react';
 import Track from './Track';
 
 export default function Tracks({ cards, selectedCard, root_url }) {
-  const playerTwoTrackNumbers = [1, 2, 3];
-  const playerOneTrackNumbers = [4, 5, 6];
 
-  const renderPlayerTwoTracks = () => {
-    return playerTwoTrackNumbers.map((playerTwoTrackNumber) => {
+  const renderTracks = (trackNumbers) => {
+    return trackNumbers.map((trackNumber) => {
       return <span>
         <Track
-          trackNumber={playerTwoTrackNumber}
-          cards={cards}
-          selectedCard={selectedCard}
-          root_url={root_url}
-        />
-      </span>
-    });
-  };
-
-  const renderPlayerOneTracks = () => {
-    return playerOneTrackNumbers.map((playerOneTrackNumber) => {
-      return <span>
-        <Track
-          trackNumber={playerOneTrackNumber}
+          trackNumber={trackNumber}
           cards={cards}
           selectedCard={selectedCard}
           root_url={root_url}
@@ -33,11 +18,11 @@ export default function Tracks({ cards, selectedCard, root_url }) {
 
   return <div>
     <span className="tracks">
-      {renderPlayerTwoTracks()}
+      {renderTracks([4, 5, 6])}
     </span>
     <br/>
     <span className="tracks">
-      {renderPlayerOneTracks()}
+      {renderTracks([1, 2, 3])}
     </span>
   </div>
 }
