17126f125e608edf0167cf8cea9a7fff21a1243a
Andy Strube <andystrube64@gmail.com>
2020-12-18 19:23:33 -0800

    Started the how to play page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'lobby', to: 'pages#lobby'
   get 'set_deck', to: 'pages#set_deck'
   get 'player_cards', to: 'pages#player_cards'
+  get 'how_to_play', to: 'pages#how_to_play'
   resources :games, only: [:create, :show, :destroy, :update]
   resources :cards, only: [:create, :destroy, :update]
 end
